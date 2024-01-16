c281eb926eaf063e18ca962c623a68705ad487b1
Gautam-2604 <gaurisariag@gmail.com>
2024-01-16 22:54:02 +0530

    file structure and database connection done

---
+++ src/app/login/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
+export default function LoginPage(){
+    const [user,setUser]=React.useState({
+        email:"",
+        password:"",
+        username:"",
+    })
+    const onsignUp=async()=>{
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>SignUp</h1>
+        </div>
+    }
+    return(
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>SignUp</h1>
+            <hr />
+            <label htmlFor="username">username</label>
+            <input 
+            type="text"
+            id="username"
+            value={user.username}
+            onChange={(e)=>setUser({...user, username:e.target.value})}
+            placeholder="username"
+            />
+            <label htmlFor="email">email</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="email"
+            type="text"
+            value={user.email}
+            onChange={(e) => setUser({...user, email: e.target.value})}
+            placeholder="email"
+            />
+        <label htmlFor="password">password</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="password"
+            type="password"
+            value={user.password}
+            onChange={(e) => setUser({...user, password: e.target.value})}
+            placeholder="password"
+            />
+            <button
+            onClick={onsignUp}
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">SignUp</button>
+            <Link href="/signup">Visit login page</Link>
+        </div>
+    )
+}
---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default function UserProfile({params}:any){
+    return(
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Profile</h1>
+            <hr />
+            <h1>Profile Page {params.id}</h1>
+        </div>
+    )
+}
